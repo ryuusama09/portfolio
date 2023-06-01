0dd539cdf151dc75fe0f2fa80690f497467d17da
ryuusama <shahh8138@gmail.com>
2023-06-01 16:08:16 +0530

    upd

--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-
 import Particle from "../Particle";
 import BlogCard from "./BlogCard"
+import opensource from "../../Assets/Projects/sunpy.png";
+import venice from "../../Assets/Projects/venice.jpg";
+import rceblog from "../../Assets/Projects/rceblog.jpg";
+import webrtc from "../../Assets/Projects/webrtc.png";
+import smawk from "../../Assets/Projects/smawk.avif";
+
 
 function Blogs() {
   return (
@@ -18,69 +23,60 @@ function Blogs() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <BlogCard
-           //   imgPath={rce}
+             imgPath={webrtc}
               isBlog={false}
-              title="RCE"
-              description=" A remote code execution engine built with NodeJs , supported by docker which is a server-worker-microservice architecture based code execution engine. It performs code virtualization , parellel code execution along with code isolation by code containerization."
-              ghLink="https://github.com/ryuusama09/"
+              title="ShallowDive WebRTC"
+            //  description=" A remote code execution engine built with NodeJs , supported by docker which is a server-worker-microservice architecture based code execution engine. It performs code virtualization , parellel code execution along with code isolation by code containerization."
+              ghLink="https://ryuusama.hashnode.dev/shallowdive-into-webrtc"
               
             />
           </Col>
 
           <Col md={4} className="project-card">
             <BlogCard
-            //  imgPath={Trendsetters}
+             imgPath={rceblog}
               isBlog={false}
-              title="TrendSetters"
-              description="A webiste that scrapes the best ongoing trends from various social media platforms and provide the best trending products to users available on flipkart. Also supports reverse image search engine which allows the user to search based on an image"
-              ghLink="https://github.com/https://github.com/ryuusama09/TrendSetters"
+              title="How to build an efficient RCE"
+             // description="A webiste that scrapes the best ongoing trends from various social media platforms and provide the best trending products to users available on flipkart. Also supports reverse image search engine which allows the user to search based on an image"
+              ghLink="https://ryuusama.hashnode.dev/remote-code-execution-engine"
              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <BlogCard
-            //  imgPath={DamselUndistressed}
-              isBlog={false}
-              title="Damsel Undistressed"
-              description="A service portal that serves as a disaster relief centre. It can detect hurricanes/storm from weather satellite images and notify users. It also acts as a community platform where organizations come up to provide relief fund in case of calamity."
-              ghLink="https://github.com/ryuusama09/Damsel-Undistressed "
+              imgPath={smawk}
+            
+              title="SMAWK alogrithm"
+            //  description="A service portal that serves as a disaster relief centre. It can detect hurricanes/storm from weather satellite images and notify users. It also acts as a community platform where organizations come up to provide relief fund in case of calamity."
+              ghLink="https://medium.com/@shahh8138/smawk-algorithm-242fa751baab"
                          
             />
           </Col>
 
           <Col md={4} className="project-card">
             <BlogCard
-             // imgPath={Sunpy}
+             imgPath={venice}
               isBlog={false}
-              title="Sunpy"
-              description="An open source library solar physics. Officially contributed for major code restructuring of code including the appropriation of the world coordinate system of astropy."
-              ghLink="https://github.com/sunpy"
+              title="Venice Sets"
+             // description="An open source library solar physics. Officially contributed for major code restructuring of code including the appropriation of the world coordinate system of astropy."
+              ghLink="https://medium.com/@shahh8138/venice-sets-fec34cad6713"
               
             />
           </Col>
 
           <Col md={4} className="project-card">
             <BlogCard
-             // imgPath={routine}
+              imgPath={opensource}
               isBlog={false}
-              title="Be-Routine"
+              title="How I jump Started into Open Source"
               description="A note down app , built with NodeJs as backend integrated with mongoDB to store data. A user-friendly UI made by html and css3. Lets"
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              ghLink="https://ryuusama.hashnode.dev/sunpy-your-way-to-solar-physics"
              // <--------Please include a demo link here
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <BlogCard
-             // imgPath={routine}
-              isBlog={false}
-              title="Stress-Tester"
-              description="Developed a stress-test system that allows the user to check the validity of a solution , by comparing your solution with the expected solution on multiple randomly generated test-cases and provides the test-case for which your code gives the incorrect output."
-              ghLink="https://github.com/"
-                    //<--------Please include a demo link here 
-            />
-          </Col>
+          
         </Row>
       </Container>
     </Container>
